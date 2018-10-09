c8b0ca406f3c3eb47d3f991a6bc182f00b27a528
nastasiiaa <sav_nastasia@mail.ru>
2018-10-09 15:50:19 +0300

    09.10.2018 15:50:19,85
    Signed-off-by: nastasiiaa <sav_nastasia@mail.ru>

---
+++ papa.bat
@@ -0,0 +1,10 @@
+git add *.cpp 
+git add *.sln 
+git add *.vcxproj 
+git add *.vcxproj.filters 
+git add *.h 
+git add *.bat 
+git add *.user 
+git commit -sm "%DATE% %TIME%" 
+git push 181_351_Konova master 
+pause
